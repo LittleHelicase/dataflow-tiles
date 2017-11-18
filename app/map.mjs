d78b01bcf05c9655fc523cad5128f88fef243af9
Max <dasmaxl@gmail.com>
2017-11-18 09:13:16 +0100

    (feature) Place tiles from tileMap.

--- app/map.mjs
+++ app/map.mjs
@@ -1,19 +1,27 @@
 
 export function loadTileMap (file, tileSize) {
   var img = new Image()
-  img.src = file
-  return new Promise((resolve) =>
-    img.addEventListener('load', resolve))
+  const imgPromise = new Promise((resolve) =>
+    img.onload = resolve)
   .then(() => {
-    const width = img.width / tileSize
-    const height = img.height / tileSize
+    const width = Math.round(img.width / tileSize)
+    const height = Math.round(img.height / tileSize)
     return {
-      drawTile: (n, ctx, canvas) => {
-        const x = n % width
-        const y = Math.floor(n / width)
-        if (y > height) throw Error(`No tile ${n} in map.`)
-        ctx.drawImage(img, x * tileSize, y * tileSize, tileSize, tileSize, 0, 0, tileSize, tileSize)
-      }
+      tileMap: img,
+      width, height,
+      tileSize
     }
   })
+  img.src = file
+  return imgPromise
+}
+
+export function drawTile(n, pt) {
+  return (ctx, map) => {
+    const x = n % map.width
+    const y = Math.floor(n / map.width)
+    if (y > map.height) throw Error(`No tile ${n} in map.`)
+    ctx.drawImage(map.tileMap, x * map.tileSize, y * map.tileSize, map.tileSize, map.tileSize,
+        pt.x * map.tileSize, pt.y * map.tileSize, map.tileSize, map.tileSize)
+  }
 }
--- app/tiles.mjs
+++ app/tiles.mjs
@@ -1,12 +1,14 @@
-import {loadTileMap} from './map.mjs'
+import {loadTileMap, drawTile} from './map.mjs'
 
 document.body.innerHTML = "<canvas id='canv'></canvas>"
 
-loadTileMap('resources/map.png', 40).then((tileMap) => {
+loadTileMap('resources/map.png', 133.333).then((tileMap) => {
   const canv = document.getElementById('canv')
-  canv.width = 200
-  canv.height = 200
+  canv.width = 300
+  canv.height = 300
 
   const ctx = canv.getContext('2d')
-  tileMap.drawTile(0, ctx)
+  drawTile(0, {x: 0, y: 0})(ctx, tileMap)
+  drawTile(14, {x: 0, y: 1})(ctx, tileMap)
+  drawTile(13, {x: 1, y: 1})(ctx, tileMap)
 })
